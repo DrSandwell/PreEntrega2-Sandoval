37c24be3e94803bc093a83d6e7e35b3e0cca6578
Sandwell <nicogonzasandoval@hotmail.com>
2023-07-20 16:46:29 -0300

    Pre entrega 2 Sandoval

--- main.js
+++ main.js
@@ -1,99 +1,83 @@
-alert("Bienvenido a Peach Wigs, el lugar donde podras comprar las mejores pelucas de cosplay anime")
+const precios = {
+    DBZ: {
+        Goku: 5000,
+        Vegeta: 6000,
+        Gohan: 7000,
+    },
+    Inuyasha: {
+        Inuyasha: 4500,
+        Kagome: 5500,
+        Kykyo: 6500,
+    },
+    "Saint Seiya": {
+        Seiya: 3700,
+        Atenea: 4700,
+        Shun: 5700,
+    },
+};
+
+const carrito = [];
 
 const comprarPelucas = () => {
-    let anime = ""
-    let peluca = ""
-    let cantidad = 0
-    let precio = 0
-    let subtotal = 0
-    let seguirComprando = true
+    let subtotal = 0;
+    let seguirComprando = true;
 
     do {
-        anime = prompt("De que anime queres comprar una peluca:¿DBZ, Inuyasha, Saint Seiya?")
+        const anime = prompt(
+            "De qué anime quieres comprar una peluca: ¿DBZ, Inuyasha, Saint Seiya?"
+        );
 
-        switch (anime) {
-            case "DBZ":
-                peluca = prompt("¿Goku(5000), Vegeta(6000), Gohan(7000)?")
-                cantidad = parseInt(prompt("¿Cuantos queres comprar?"))
-                switch (peluca) {
-                    case "Goku":
-                        precio = 5000
-                        break;
-                    case "Vegeta":
-                        precio = 6000
-                        break;
-                    case "Gohan":
-                        precio = 7000
-                        break;
-                    default:
-                        alert("Algun valor ingresado no es valido")
-                        precio = 0
-                        cantidad = 0
+        if (anime in precios) {
+            const pelucasAnime = precios[anime];
+            const opcionesPeluca = Object.keys(pelucasAnime)
+                .map((precio) => `${precio} ($${pelucasAnime[precio]})`)
+                .join(", ");
+            const peluca = prompt(`¿${opcionesPeluca}?`);
+
+            if (peluca in pelucasAnime) {
+                const cantidad = parseInt(prompt("¿Cuántas quieres comprar?"));
+
+                if (isNaN(cantidad) || cantidad <= 0) {
+                    alert("Debes ingresar una cantidad válida");
+                } else {
+                    const precio = pelucasAnime[peluca];
+                    subtotal += precio * cantidad;
+                    carrito.push({
+                        anime,
+                        peluca,
+                        cantidad,
+                        precio,
+                    });
                 }
-                break;
-            case "Inuyasha":
-                peluca = prompt("¿Inuyasha(4500), Kagome(5500), Kykyo(6500)?")
-                cantidad = parseInt(prompt("¿Cuantos queres comprar?"))
-                switch (peluca) {
-                    case "Inuyasha":
-                        precio = 4500
-                        break;
-                    case "Kagome":
-                        precio = 5500
-                        break;
-                    case "Kykyo":
-                        precio = 6500
-                        break;
-                    default:
-                        alert("Algun valor ingresado no es valido")
-                        precio = 0
-                        cantidad = 0
-                }
-                break;
-            case "Saint Seiya":
-                peluca = prompt("Seiya(3700), Atenea(4700), Shun(5700)?")
-                cantidad = parseInt(prompt("¿Cuantos queres comprar?"))
-                switch (peluca) {
-                    case "Seiya":
-                        precio = 3700
-                        break;
-                    case "Atenea":
-                        precio = 4700
-                        break;
-                    case "Shun":
-                        precio = 5700
-                        break;
-                    default:
-                        alert("Algun valor ingresado no es valido")
-                        precio = 0
-                        cantidad = 0
-                }
-                break;
-            default:
-                alert("Algun valor ingresado no es valido")
-                precio = 0
-                cantidad = 0
+            } else {
+                alert("Valor ingresado no válido");
+            }
+        } else {
+            alert("Valor ingresado no válido");
         }
 
-        subtotal += precio * cantidad 
-
-        console.log(anime);
-        console.log(peluca);
-        console.log(cantidad);
-
-        seguirComprando = confirm("¿Desea seguir comprando?")
+        seguirComprando = confirm("¿Desea seguir comprando?");
     } while (seguirComprando);
-    return subtotal
-}
 
-/* const validarCantidad = (cantidad) => {
-    while (Number.isNaN(cantidad) || cantidad === 0){
-        alert("Debe agregar una cantidad valida!!!")
-        cantidad = parseInt(prompt("¿Cuantos queres comprar?"))
-    }
-}
- */
-let total = comprarPelucas()
-console.log(total);
+    return subtotal;
+};
 
-alert ("El total de su compra es: $" +total)
+const mostrarCarrito = () => {
+    console.log("Carrito de compra:");
+    carrito.forEach((item) => {
+        console.log(
+            `${item.cantidad} peluca(s) de ${item.peluca} del anime ${item.anime} - Precio unitario: $${item.precio}`
+        );
+    });
+};
+
+const total = comprarPelucas();
+alert("El total de su compra es: $" + total);
+
+const Reinicio = document.getElementById("Reinicio");
+Reinicio.addEventListener("click", () => {
+    carrito.length = 0;
+    const total = comprarPelucas();
+    alert("El total de su compra es: $" + total);
+    mostrarCarrito();
+});
